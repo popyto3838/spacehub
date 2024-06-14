f5b056892a565cd59bb10af13764b9766bc97595
dlehddud60 <dlehddud60@naver.com>
2024-06-14 16:27:04 +0900

    프론트의 css main width 변경

--- frontend/src/component/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import { Box, Text } from "@chakra-ui/react";
-
-export function Footer() {
-  return (
-    <Box as="footer" bg="gray.800" color="white" p={4}>
-      <Text textAlign="center">Copyright &copy; Spacehub</Text>
-    </Box>
-  );
-}
