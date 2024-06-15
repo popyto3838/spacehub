320d9d014266df76ad21539e288e4d2ec01294ef
KwonSanta <KwonSanta@gmail.com>
2024-06-16 02:19:15 +0900

    #16. Material UI 제거

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,48 +21,43 @@ import {NaverLoginHandler} from "./page/member/NaverLoginHandler.jsx";
 import {LoginProvider} from "./component/LoginProvider.jsx";
 import {SpaceType} from "./page/admin/space_config/SpaceType.jsx";
 
-// MUI 테마 설정
-const theme = createTheme();
-
 function App() {
-    const router = createBrowserRouter([
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+      children: [
         {
-            path: "/",
-            element: <Home/>,
-            children: [
-                {
-                    path: "reserve",
-                    element: <Reserve/>
-                },
-                {index: true, element: <MainPage/>},
-                {path: "board/write", element: <BoardWrite/>},
-                {path: "board/list", element: <BoardList/>},
-                {path: "board/:boardId", element: <BoardView/>},
-                {path: "board/:boardId/edit", element: <BoardEdit/>},
-                {path: "space/register", element: <RegisterStepper/>},
-                {path: "space/option", element: <SpaceOption/>},
-                {path: "space/type", element: <SpaceType/>},
-                {path: "member/signup", element: <MemberSignup/>},
-                {path: "member/naver", element: <NaverLogin/>},
-                {path: "member/list", element: <MemberList/>},
-                {path: "member/:memberId", element: <MemberInfo/>},
-                {path: "member/login", element: <MemberLogin/>},
-                {path: "member/edit/:memberId", element: <MemberEdit/>},
-                {path: "auth/naverlogin", element: <NaverLoginHandler/>},
-            ],
+          path: "reserve",
+          element: <Reserve/>
         },
-    ]);
-    return (
-        <>
-            <ThemeProvider theme={theme}>
-                <LoginProvider>
-                    <ChakraProvider>
-                        <RouterProvider router={router}/>
-                    </ChakraProvider>
-                </LoginProvider>
-            </ThemeProvider>
-        </>
-    );
+        {index: true, element: <MainPage/>},
+        {path: "board/write", element: <BoardWrite/>},
+        {path: "board/list", element: <BoardList/>},
+        {path: "board/:boardId", element: <BoardView/>},
+        {path: "board/:boardId/edit", element: <BoardEdit/>},
+        {path: "space/register", element: <RegisterStepper/>},
+        {path: "space/option", element: <SpaceOption/>},
+        {path: "space/type", element: <SpaceType/>},
+        {path: "member/signup", element: <MemberSignup/>},
+        {path: "member/naver", element: <NaverLogin/>},
+        {path: "member/list", element: <MemberList/>},
+        {path: "member/:memberId", element: <MemberInfo/>},
+        {path: "member/login", element: <MemberLogin/>},
+        {path: "member/edit/:memberId", element: <MemberEdit/>},
+        {path: "auth/naverlogin", element: <NaverLoginHandler/>},
+      ],
+    },
+  ]);
+  return (
+    <>
+      <LoginProvider>
+        <ChakraProvider>
+          <RouterProvider router={router}/>
+        </ChakraProvider>
+      </LoginProvider>
+    </>
+  );
 }
 
 
