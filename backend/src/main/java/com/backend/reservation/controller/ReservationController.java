3e40e0fa968f143e6437bd49fb17258ddc6a5a37
dlehddud60 <dlehddud60@naver.com>
2024-06-17 15:47:26 +0900

    #34 예약 CRUD 백엔드 작업

---
+++ backend/src/main/java/com/backend/reservation/controller/ReservationController.java
@@ -0,0 +1,51 @@
+package com.backend.reservation.controller;
+
+import com.backend.reservation.domain.Reservation;
+import com.backend.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservation")
+@RequiredArgsConstructor
+@Log4j2
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @GetMapping("/list")
+    public List<Reservation> list() {
+        log.info("=========리스트 호출==============");
+        return reservationService.list();
+    }
+
+    @GetMapping("/{reservationId}")
+    public ResponseEntity view(@PathVariable Integer reservationId) {
+        Reservation reservation = reservationService.view(reservationId);
+        if (reservation == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(reservation);
+    }
+
+    @PostMapping("/write")
+    public void write(Reservation reservation) {
+        reservationService.insert(reservation);
+    }
+
+    @PutMapping("/update")
+    public void update(Reservation reservation) {
+        reservationService.update(reservation);
+    }
+
+    @DeleteMapping("/delete/{reservationId}")
+    public void delete(@PathVariable Integer reservationId) {
+        reservationService.delete(reservationId);
+    }
+
+}
---
+++ backend/src/main/java/com/backend/reservation/domain/Reservation.java
@@ -0,0 +1,21 @@
+package com.backend.reservation.domain;
+
+import com.backend.reservation.domain.status.ReservationStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
+public class Reservation {
+    private Integer reservationId;
+    private Integer spaceId;
+    private Integer memberId;
+    private Timestamp date;
+    private String  startTime;
+    private String  endTime;
+    private ReservationStatus status;
+    private Timestamp inputDt;
+    private Timestamp updateDt;
+}
