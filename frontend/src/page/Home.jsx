cd3830c8cecfaecee06e04bfc010619c2ad67f87
dlehddud60 <dlehddud60@naver.com>
2024-06-17 10:05:28 +0900

    #32 content컴포넌트 제거 사이드바 css변경

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -2,18 +2,20 @@ import {Box, Flex} from "@chakra-ui/react";
 import Header from "../common/Header.jsx";
 import Footer from "../common/Footer.jsx";
 import '/public/css/Home.css';
-import Content from "../common/Content.jsx";
+import {Outlet} from "react-router-dom";
 
 export function Home() {
-  return (
-    <Flex>
-      <Box flex="1">
-        <Header></Header>
-        <Box>
-          <Content></Content>
-          <Footer/>
-        </Box>
-      </Box>
-    </Flex>
-  );
+    return (
+        <Flex>
+            <Box flex="1">
+                <Header/>
+                <Box>
+                    <div className="content">
+                        <Outlet/>
+                    </div>
+                </Box>
+                <Footer/>
+            </Box>
+        </Flex>
+    );
 }
