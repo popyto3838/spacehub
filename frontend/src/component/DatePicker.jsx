765f8a8977917606c02269540c099dbe6cee1cce
dlehddud60 <dlehddud60@naver.com>
2024-06-19 14:33:02 +0900

    #37 결제 API 결제시 reservation status값 결제 완료로 바꾸기

--- frontend/src/component/DatePicker.jsx
+++ frontend/src/component/DatePicker.jsx
@@ -10,7 +10,6 @@ const Calendar = () => {
         '1월', '2월', '3월', '4월', '5월', '6월',
         '7월', '8월', '9월', '10월', '11월', '12월'
     ];
-    console.log("======================", currentDate)
     const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
 
     const getDaysInMonth = (year, month) => {
