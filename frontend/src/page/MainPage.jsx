f394d3117c41a654d100a31d363fdb130f9a8644
KwonSanta <KwonSanta@gmail.com>
2024-06-21 11:39:43 +0900

    #58. 메인페이지 카드 클릭시 view페이지로 이동

--- frontend/src/page/MainPage.jsx
+++ frontend/src/page/MainPage.jsx
@@ -3,10 +3,13 @@ import {Box, Grid, GridItem, Text, VStack} from "@chakra-ui/react";
 import '../../../frontend/public/css/MainPage.css';
 import axios from "axios";
 import SpaceCard from "./space/SpaceCard.jsx";
+import {useNavigate} from "react-router-dom";
 
 export function MainPage() {
   const [spaceTypes, setSpaceTypes] = useState([]);
   const [spaces, setSpaces] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     // type_list 정보를 가져오는 API 호출
@@ -28,6 +31,11 @@ export function MainPage() {
       });
   }, []);
 
+  function handleCardClick(spaceId) {
+    console.log(spaceId)
+    navigate(`/space/${spaceId}`)
+  }
+
   return (
     <>
       <Box py={5}>
@@ -48,7 +56,7 @@ export function MainPage() {
                   justifyContent="center"
                   _hover={{bg: 'gray.300'}}
                 >
-                  {/* Icon을 추가할 경우 여기에 추가 */}
+                  <Box>아이콘이미지 위치</Box>
                 </Box>
                 <Text _hover={{color: 'teal.500'}}>{type.name}</Text>
               </VStack>
@@ -60,7 +68,9 @@ export function MainPage() {
         <Text fontSize="2xl" fontWeight="bold">공간 목록</Text>
         <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={6} mt={4}>
           {spaces.map(space => (
-            <GridItem key={space.id}>
+            <GridItem key={space.id} onClick={() => {
+              handleCardClick(space.spaceId)
+            }}>
               <SpaceCard space={space}/>
             </GridItem>
           ))}
