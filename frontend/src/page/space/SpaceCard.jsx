b675f8b55aacb234ede5912486126eaa5548e93b
KwonSanta <KwonSanta@gmail.com>
2024-06-22 17:49:05 +0900

    #58. 공간 리스트 이미지 레이아웃 맞추기

--- frontend/src/page/space/SpaceCard.jsx
+++ frontend/src/page/space/SpaceCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardBody, CardFooter, CardHeader, Flex, Heading, Image, Text} from '@chakra-ui/react';
+import {Box, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Image, Text} from '@chakra-ui/react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faComment, faHeart, faStar, faThumbsUp} from "@fortawesome/free-solid-svg-icons";
 
@@ -9,33 +9,34 @@ const SpaceCard = ({space, file}) => {
     : 'http://via.placeholder.com/1000.jpg';
 
   return (
-    <Card maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-      <CardHeader>
-        <Flex justify='space-between' align='center'>
-          <Heading size='md'>{space.title}</Heading>
-          <FontAwesomeIcon icon={faHeart}/>
-          <Text>{space.type}</Text>
-        </Flex>
-      </CardHeader>
-      <CardBody>
+    <Card maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' position='relative' height='350px'>
+      <Box position='absolute' top={2} right={2} zIndex={1}>
+        <FontAwesomeIcon icon={faHeart} />
+      </Box>
+      <CardBody p={0}>
         <Image
           src={thumbnailPath}
           alt={space.title}
-          boxSize="200px"
-          objectFit="contain"
+          width="100%"
+          height="100%"
+          objectFit="cover"
         />
       </CardBody>
-      <CardFooter>
-        <Flex align='center' mt='2'>
-          <FontAwesomeIcon icon={faStar}/>
-          <Text ml='1'>별점</Text>
-        </Flex>
-        <Text mt='2'>₩{space.price} / 시간</Text>
-        <Flex mt='2' justify='space-between'>
-          <FontAwesomeIcon icon={faComment}/>
-          리뷰
-          <FontAwesomeIcon icon={faThumbsUp}/>
-          좋아요
+      <CardFooter p={2} height="20%">
+        <Flex direction='column' align='start'>
+          <Heading size='md'>{space.title}</Heading>
+          <Text>{space.type}</Text>
+          <Flex align='center' mt='2'>
+            <FontAwesomeIcon icon={faStar} />
+            <Text ml='1'>별점</Text>
+          </Flex>
+          <Text mt='2'>₩{space.price} / 시간</Text>
+          <Flex mt='2' justify='space-between'>
+            <FontAwesomeIcon icon={faComment} />
+            리뷰
+            <FontAwesomeIcon icon={faThumbsUp} />
+            좋아요
+          </Flex>
         </Flex>
       </CardFooter>
     </Card>
